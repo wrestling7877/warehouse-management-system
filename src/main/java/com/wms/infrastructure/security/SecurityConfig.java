2f629dd52324a6bd5ac03d374423997ffb098263
wrestling7877 <bekuzodo7877@gmail.com>
2026-06-07 17:25:11 +0900

    feat: add authentication dto layer

--- src/main/java/com/wms/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/wms/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.wms.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +16,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
