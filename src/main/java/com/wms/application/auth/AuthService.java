4fc9c01413ded4ce22ac31b39b9efd51ca760cbf
wrestling7877 <bekuzodo7877@gmail.com>
2026-06-11 18:18:50 +0900

    feat: implement jwt token generation

--- src/main/java/com/wms/application/auth/AuthService.java
+++ src/main/java/com/wms/application/auth/AuthService.java
@@ -1,10 +1,13 @@
 package com.wms.application.auth;
 
+import com.wms.infrastructure.security.JwtService;
 import com.wms.presentation.auth.dto.LoginRequest;
 import com.wms.presentation.auth.dto.LoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,16 +16,25 @@ public class AuthService {
 
     private final AuthenticationManager authenticationManager;
 
+    private final JwtService jwtService;
+
     public LoginResponse login(LoginRequest request) {
 
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getEmail(),
+                                request.getPassword()
+                        )
+                );
 
-        return new LoginResponse("temporary-token");
+        UserDetails userDetails =
+                (UserDetails) authentication.getPrincipal();
+
+        String token =
+                jwtService.generateToken(userDetails);
+
+        return new LoginResponse(token);
     }
 
 }
---
+++ src/main/java/com/wms/infrastructure/security/JwtService.java
@@ -0,0 +1,34 @@
+package com.wms.infrastructure.security;
+
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    public String generateToken(UserDetails userDetails) {
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
